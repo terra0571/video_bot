3f5355fa0d4540e061f948ad556b3d8e2f4833eb
terra0571 <7u99er@GMAIL.COM>
2025-05-13 15:30:25 +0500

    Update downloader.py

--- downloader.py
+++ downloader.py
@@ -2,11 +2,18 @@ from pytube import YouTube
 import os
 
 def download_youtube_video(url):
-    yt = YouTube(url)
-    stream = yt.streams.filter(progressive=True, file_extension="mp4").order_by("resolution").desc().first()
-    
-    if not os.path.exists("downloads"):
-        os.makedirs("downloads")
-    
-    file_path = stream.download(output_path="downloads")
-    return file_path, yt.title
+    try:
+        yt = YouTube(url)
+        stream = yt.streams.filter(progressive=True, file_extension="mp4").order_by("resolution").desc().first()
+
+        if stream is None:
+            raise Exception("❌ Video oqim topilmadi (format mavjud emas).")
+
+        if not os.path.exists("downloads"):
+            os.makedirs("downloads")
+
+        file_path = stream.download(output_path="downloads")
+        return file_path, yt.title
+
+    except Exception as e:
+        raise Exception(f"❌ YouTube videoni yuklab bo‘lmadi:\n{str(e)}")
