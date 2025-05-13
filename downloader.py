2774a212e458b9c0b88c955a34520b1a577cbbea
terra0571 <7u99er@GMAIL.COM>
2025-05-13 15:33:28 +0500

    Update downloader.py

--- downloader.py
+++ downloader.py
@@ -1,19 +1,18 @@
-from pytube import YouTube
 import os
+from yt_dlp import YoutubeDL
 
 def download_youtube_video(url):
-    try:
-        yt = YouTube(url)
-        stream = yt.streams.filter(progressive=True, file_extension="mp4").order_by("resolution").desc().first()
+    if not os.path.exists("downloads"):
+        os.makedirs("downloads")
 
-        if stream is None:
-            raise Exception("❌ Video oqim topilmadi (format mavjud emas).")
+    ydl_opts = {
+        'outtmpl': 'downloads/%(title)s.%(ext)s',
+        'format': 'mp4',
+        'quiet': True,
+        'noplaylist': True,
+    }
 
-        if not os.path.exists("downloads"):
-            os.makedirs("downloads")
-
-        file_path = stream.download(output_path="downloads")
-        return file_path, yt.title
-
-    except Exception as e:
-        raise Exception(f"❌ YouTube videoni yuklab bo‘lmadi:\n{str(e)}")
+    with YoutubeDL(ydl_opts) as ydl:
+        info = ydl.extract_info(url, download=True)
+        filename = ydl.prepare_filename(info)
+        return filename, info.get("title", "Video")
